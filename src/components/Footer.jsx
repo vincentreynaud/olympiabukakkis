2382f8f23633a1e0c08b7bf71e95f1f979bf9ad0
Vincent <vincentreynaud.de@gmail.com>
2019-06-19 12:07:44 +0200

    optimise footer behaviour

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,8 +31,22 @@ class Footer extends Component {
     };
   }
 
-  isPageBottom = () => {
-    return window.innerHeight + window.scrollY >= document.body.offsetHeight;
+  handleScroll = e => {
+    const xMove = e.deltaX;
+    const yMove = e.deltaY;
+
+    // Excludes X moves
+    if (Math.abs(yMove) > Math.abs(xMove)) {
+      this.setState({ scrollDown: e.deltaY > 0 });
+    }
+
+    this.toggleFooter();
+    // FIX: need a working requestAnimationFrame setup!
+    // if (!this.state.updated) window.requestAnimationFrame(this.toggleFooter);
+    // this.setState({ updated: true });
+
+    // reset checks
+    this.setState({ scrollDown: null });
   };
 
   handleTouchStart = e => {
@@ -50,9 +64,7 @@ class Footer extends Component {
     const xDiff = this.state.touchStartX - xUp;
     const yDiff = this.state.touchStartY - yUp;
 
-    // exclude x moves
     if (Math.abs(yDiff) > Math.abs(xDiff)) {
-      console.log("Y Swipe", yDiff);
       this.setState({ scrollDown: yDiff > 0 });
     }
 
@@ -61,27 +73,18 @@ class Footer extends Component {
     this.setState({ touchStartX: null, touchStartY: null, scrollDown: null });
   };
 
-  handleScroll = e => {
-    this.setState({ scrollDown: e.deltaY >= 0 });
-
-    this.toggleFooter();
-    // if (!this.state.updated) window.requestAnimationFrame(this.toggleFooter);
-    this.setState({ updated: true });
-
-    // reset checks
-    this.setState({ scrollDown: null });
+  isPageBottom = () => {
+    return window.innerHeight + window.scrollY >= document.body.offsetHeight;
   };
 
   toggleFooter = () => {
-    // console.log("window inner height + scroll y", window.innerHeight + window.scrollY);
-    // console.log("doc body offsetheight: ", document.body.offsetHeight);
     if (this.state.scrollDown && this.isPageBottom()) {
       this.show();
     } else if (!this.state.scrollDown) {
       this.hide();
     }
 
-    this.setState({ updated: false });
+    // this.setState({ updated: false });
   };
 
   show = () => {
@@ -101,14 +104,15 @@ class Footer extends Component {
   render() {
     return (
       <footer className="w-100 footer-hide row justify-content-between" id="footer">
-        <p className="mb-1 col-12 col-sm text-nowrap">Lecken &copy; {new Date().getFullYear()}</p>
+        <p className="mb-1 col-12 col-sm text-nowrap">Olympia Bukkakis &copy; {new Date().getFullYear()}</p>
         <p className="footer-credit">
           With <span className="red">&#10084;</span> by{" "}
-          <a className="text-nowrap" href="https://www.instagram.com/claudiustoximus" target="_blank">
-            Claudio Tocco
-          </a>{" "}
-          &amp;{" "}
-          <a className="text-nowrap" href="https://www.instagram.com/collectiveanxiety/" target="_blank">
+          <a
+            rel="noopener noreferrer"
+            className="text-nowrap"
+            href="https://www.instagram.com/collectiveanxiety/"
+            target="_blank"
+          >
             Vincent Reynaud
           </a>
         </p>
@@ -118,43 +122,3 @@ class Footer extends Component {
 }
 
 export default Footer;
-
-// var lastScrollTop = 0;
-
-// element.addEventListener("scroll", handleScroll2, false);
-
-// function handleScroll2() { // or window.addEventListener("scroll"....
-//    var currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-//    if (currentScrollTop > lastScrollTop){
-//       console.log("scrolled down")
-//     } else {
-//       console.log("scrolled up")
-//    }
-//    lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop; // For Mobile or negative scrolling
-// }
-
-// console.log(e.target.scrollHeight);
-// console.log(document.documentElement.scrollTop);
-
-/* {
-  for (let i = 0; i < e.changedTouches.length; i++) {
-    console.log(e.touches);
-    console.log(e.changedTouches);
-    console.log("changedTouches[" + i + "].identifier = " + e.changedTouches[i].identifier);
-  }
-}; */
-
-/*
-if (Math.abs(xDiff) > Math.abs(yDiff)) {
-      if (xDiff > 0) {
-        // left swipe
-      } else {
-        // right swipe
-      }
-    } else {
-      if (yDiff > 0) {
-        // up swipe 
-      } else {
-        // down swipe 
-      }
- */
