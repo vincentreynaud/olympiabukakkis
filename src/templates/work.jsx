0b21e1fbda73f65dd45a0057af571676d6a32f12
Vincent <vincentreynaud.de@gmail.com>
2019-06-23 13:45:02 +0200

    minor edits

--- src/templates/work.jsx
+++ src/templates/work.jsx
@@ -13,8 +13,8 @@ export default ({ data }) => {
       <SEO title={post.frontmatter.title} />
       <Layout>
         <Close />
-        <div>{post.frontmatter.title}</div>
-        <div>{post.frontmatter.date}</div>
+        <h1>{post.frontmatter.title}</h1>
+        <h2>{post.frontmatter.date}</h2>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </Layout>
     </>
