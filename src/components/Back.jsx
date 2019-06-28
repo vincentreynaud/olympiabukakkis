e82757527469f8ce4c62b9d9192c01b1d71a82a8
Vincent <vincentreynaud.de@gmail.com>
2019-06-28 10:22:45 +0200

    change nav icons

--- src/components/Back.jsx
+++ src/components/Back.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Back() {
   return (
     <button className="back top right" onClick={() => window.history.back()}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M427 234.625H167.296l119.702-119.702L256 85 85 256l171 171 29.922-29.924-118.626-119.701H427v-42.75z" />
       </svg>
     </button>
   );
--- src/components/ToTop.jsx
+++ src/components/ToTop.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function ToTop() {
   return (
     <button className="to-top bottom right" onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-        <path d="M34.9 289.5l-22.2-22.2c-9.4-9.4-9.4-24.6 0-33.9L207 39c9.4-9.4 24.6-9.4 33.9 0l194.3 194.3c9.4 9.4 9.4 24.6 0 33.9L413 289.4c-9.5 9.5-25 9.3-34.3-.4L264 168.6V456c0 13.3-10.7 24-24 24h-32c-13.3 0-24-10.7-24-24V168.6L69.2 289.1c-9.3 9.8-24.8 10-34.3.4z" />
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+        <path d="M277.375 427V167.296l119.702 119.702L427 256 256 85 85 256l29.924 29.922 119.701-118.626V427h42.75z" />
       </svg>
     </button>
   );
